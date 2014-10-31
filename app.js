71e4748a2132518979ec6bf8139b7acf90ac7df3
Hüseyin Uslu <shalafiraistlin@gmail.com>
2014-10-31 03:10:31 +0200

    tiny server improvements

--- app.js
+++ app.js
@@ -2,20 +2,43 @@
 
 var server = net.createServer(
     {
-         allowHalfOpen: false
+        allowHalfOpen: false
     },
-    function(socket) {
+    function (socket) {
         handleSocket(socket);
     });
 
 server.listen(81, function () {
-    console.log('server listening');
+    console.log('Server listening on ' + server.address().address + ':' + server.address().port);
 });
 
 handleSocket = function (socket) {
-    console.log('client connected');
-
+    console.log('client connected: ' + socket.remoteAddress + ":" + socket.remotePort);
+    socket.setKeepAlive(true);
+    
+    var client = new StratumClient({
+        socket: socket
+    });
+    
+    client.init();
+    
     socket.on('end', function () {
         console.log('client disconnected');
     });
 };
+
+var StratumClient = function (options) {
+    
+    this.socket = options.socket;
+    this.remoteAddress = options.socket.remoteAddress;
+    this.lastActivity = Date.now();
+    
+    this.init = function init() {
+        setupSocket();
+    };
+    
+    function setupSocket() {
+        var socket = options.socket;
+        socket.setEncoding('utf8');
+    }
+}
