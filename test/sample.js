13714ab56f428fb5415c7adf131f9ad60749ffe7
bonesoul <shalafiraistlin@gmail.com>
2014-11-10 15:45:51 +0200

    Adding a test system.

---
+++ test/sample.js
@@ -0,0 +1,14 @@
+﻿describe('sample', function() {
+
+    var data;
+
+    before(function(done) {
+        data = {
+            name: 'sample'
+        };
+    });
+
+    it('should', function() {
+        data.should.have.property('name', 'sample');
+    });
+});
